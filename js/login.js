51323cd8829f09e3e98b25ef9fb89e7e30e5177c
dbrwd <ruben9752@hotmail.com>
2022-11-13 17:37:25 -0300

    Removidas funciones innecesarias
    Se removieron todas las funciones de validación mediante JS que ya no son necesarias para validar el inicio de sesión.

--- js/login.js
+++ js/login.js
@@ -1,24 +1,13 @@
 
-// Evento que llama la función de validación de campos cuando detecta que se presiona el botón "Ingresar".
-document.getElementById("login-Btn").addEventListener("click", function() {
-    validarIngreso();
-});
- 
+// Redirige al usuario a la página principal del E-commerce tras login con Google Oauth.
+function redirigir() {
+  localStorage.setItem("userID", "user@oauth.com");
+  window.location.href="postLoginIndex.html";
 
-// Función que verifica los campos para redirigir al inicio, o mostrar un error en pantalla en caso que estén vacíos.
-function validarIngreso() {
-    if (document.getElementById("email-login").value != "" && document.getElementById("passwd-login").value != "") {
-        redirigir();
-    }
-    else {
-        document.querySelector("#error-empty").innerText = "Rellene los campos vacíos";
-        document.querySelector("#error-empty").style.color ="rgb(255,0,0)";
-    }
 }
 
-// Función que redirige al usuario a la página principal del E-commerce.
-function redirigir() {
-    localStorage.setItem("userID", document.getElementById("email-login").value);
-    window.location.href="postLoginIndex.html";
-
+// Guarda el email del usuario logeado en LocalStorage.
+function guardarEmail() {
+  localStorage.setItem("userID", document.getElementById("email-login").value);
+  localStorage.setItem("userProfilePhoto", "");
 }
